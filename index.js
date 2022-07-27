159725d810d1683c378e06dfe6537ef2a3870393
“lukechaplin” <lukechaplin@gmail.com>
2022-07-27 15:31:31 +0100

    added first test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const Sum = () => ((total = 1 + 2), console.log(total));
+const sum = () => (total = 1 + 2);
 
-Sum();
+sum();
 
-function Sum2() {
+module.exports = sum;
+
+/*function Sum2() {
 	(total2 = 1 + 2), console.log(total2);
 }
 
@@ -10,4 +12,4 @@ Sum2();
 
 hello = () => console.log("hello world!");
 
-hello();
+hello();*/
---
+++ sum.test.js
@@ -0,0 +1,5 @@
+sum = require("./index");
+
+test("adds 1 + 2 to equal 3", () => {
+	expect(sum()).toBe(3);
+});
